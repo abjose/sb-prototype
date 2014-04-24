d11fa15d7c15e1a41643eec26c6dd9430d097776
Alex B. Jose <ajose@caltech.edu>
2014-04-24 00:30:35 -0700

    add more buttons and stuff

--- more_databasey/gui.py
+++ more_databasey/gui.py
@@ -8,11 +8,8 @@ code initally taken from:
 http://matplotlib.org/examples/widgets/slider_demo.html
 
 TODO
-- get to display all three graphs
-- add sliders for number nodes, connect prob, population
-- add sliders for honesty, participation, misbelief
-- add 'reset' button
 - add 'do 10 steps' button (unless can slide back and forth in time)
+- add something displaying quantitative difference between truth and graph
 """
 
 fig, ax = plt.subplots()
@@ -34,7 +31,7 @@ axpart   = plt.axes([0.15, 0.19, 0.65, 0.02], axisbg=axcolor)
 axmisb   = plt.axes([0.15, 0.22, 0.65, 0.02], axisbg=axcolor)
 
 spop   = Slider(axpop,   'Population',   1,  500,   valinit=20, valfmt='%1.0f')
-sprob  = Slider(axprob,  'Probability',  0., 1.,    valinit=0.6)
+sprob  = Slider(axprob,  'Probability',  0., 1.,    valinit=0.3)
 snodes = Slider(axnodes, 'Nodes',        1,  100,   valinit=5,  valfmt='%1.0f')
 
 shonest = Slider(axhonest, 'Honesty',       0., 1., valinit=.75)
@@ -47,39 +44,33 @@ size = 5
 connect_prob = 0.2
 s = Site(pop, size, connect_prob)
 for _ in range(100): s.tick()
-#s.show_difference()
-
 s.test_draw(ax1, ax2, ax3)
 
-def update(val):
-    #amp = samp.val
-    #freq = sfreq.val
-    #print int(round(freq))
-    #l.set_ydata(amp*np.sin(2*np.pi*freq*t))
-    #s = amp*np.sin(2*np.pi*freq*t)
-    #plt.plot(t,s, lw=2, color='red')
-    #l.plot(t,s, lw=2, color='red')
-    #fig.draw()
+axrestart = plt.axes([0.65, 0.015, 0.1, 0.035])
+brestart = Button(axrestart, 'Restart', color=axcolor, hovercolor='0.975')
+def restart(event):
+    s = Site(int(round(spop.val)), 
+             int(round(snodes.val)), 
+             sprob.val)
+    for _ in range(10): s.tick()
     ax1.cla()
     ax2.cla()
     ax3.cla()
     s.test_draw(ax1, ax2, ax3)
     fig.canvas.draw_idle()
-#sfreq.on_changed(update)
-#samp.on_changed(update)
+    #spop.reset()
+brestart.on_clicked(restart)
 
-resetax = plt.axes([0.65, 0.015, 0.1, 0.035])
-button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
-def reset(event):
-    sfreq.reset()
-    samp.reset()
-button.on_clicked(reset)
 
-#rax = plt.axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
-#radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
-#def colorfunc(label):
-#    l.set_color(label)
-#    fig.canvas.draw_idle()
-#radio.on_clicked(colorfunc)
+axstep = plt.axes([0.53, 0.015, 0.1, 0.035])
+bstep = Button(axstep, 'Step 10', color=axcolor, hovercolor='0.975')
+def step(event):
+    for _ in range(50): s.tick()
+    ax1.cla()
+    ax2.cla()
+    ax3.cla()
+    s.test_draw(ax1, ax2, ax3)
+    fig.canvas.draw_idle()
+bstep.on_clicked(step)
 
 plt.show()
